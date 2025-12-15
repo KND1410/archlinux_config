cf96ceedc5cf29a17da1fc1b700b651338ca5e8a
Nguyen Dang Khoi <khoind1410@gmail.com>
2025-12-16 00:45:08 +0700

    Config

--- fastfetch/config.fish
+++ fastfetch/config.fish
@@ -1,29 +1,29 @@
-# ملف config.fish
+# config.fish file
 
 set LOGO_DIR $HOME/.config/fastfetch/logos
 set USED_FILE $HOME/.config/fastfetch/used_logos.txt
 
 function fastfetch_random
     if not test -d $LOGO_DIR
-        echo "مجلد الشعارات غير موجود: $LOGO_DIR"
+        echo "Logos directory does not exist: $LOGO_DIR"
         return
     end
 
     set all_logos (ls $LOGO_DIR)
 
     if test (count $all_logos) -eq 0
-        echo "لا يوجد شعارات في المجلد $LOGO_DIR"
+        echo "No logos found in directory $LOGO_DIR"
         return
     end
 
-    # قراءة الشعارات المستخدمة من الملف إذا موجود
+    # Read used logos from file if it exists
     if test -f $USED_FILE
         set used_logos (cat $USED_FILE)
     else
         set used_logos
     end
 
-    # إيجاد الشعارات غير المستخدمة
+    # Find unused logos
     set unused_logos
     for logo in $all_logos
         if not contains $logo $used_logos
@@ -31,24 +31,24 @@ function fastfetch_random
         end
     end
 
-    # إذا تم استخدام كل الشعارات، إعادة التعيين
+    # If all logos have been used, reset
     if test (count $unused_logos) -eq 0
         set unused_logos $all_logos
-        # مسح الملف
+        # Clear the file
         echo -n > $USED_FILE
         set used_logos
     end
 
-    # اختيار شعار عشوائي
+    # Choose a random logo
     set rand_index (random 1 (count $unused_logos))
     set LOGO $unused_logos[$rand_index]
 
-    # تحديث ملف الشعارات المستخدمة
+    # Update used logos file
     echo $LOGO >> $USED_FILE
 
-    # تشغيل fastfetch
+    # Run fastfetch
     fastfetch --logo $LOGO_DIR/$LOGO
 end
 
-# استدعاء الدالة مباشرة
+# Call the function immediately
 fastfetch_random
